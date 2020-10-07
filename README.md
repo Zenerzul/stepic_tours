0eb0f41375d161427f6d74b02ac1d07792e3906e
Zenerzul <suchalko-oleg@mail.ru>
2020-10-08 00:07:51 +0300

    Project added to GIt

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# stepic_tours
-Project 1 for Django course on Stepic
+# Stepic_tours
+Welcome!
+
+This is my second project for [Django course on Stepic](<https://stepik.org/course/63298>). 
+
+## What's new here?
+* Huge choice of tours
+* Added site navigation
+* Variation of nights and prices on Tour pages
+
+and much more!
+
+Enjoy!
+
+#### P.S. All requirements are listed in reqyirements.txt file
--- tours/views.py
+++ tours/views.py
@@ -1,19 +1,79 @@
-# from django.conf import settings
-# from django.http import HttpResponse, Http404
+from random import sample
+
 from django.shortcuts import render
 from django.views import View
+
+from tours.data import title, subtitle, description, departures, tours
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        tours_number = sample(range(1, 17), 6)
+        tours_random_six = {}
+        for tour in tours:
+            if tour in tours_number:
+                tours_random_six[tour] = tours[tour]
+        return render(request, 'tours/index.html', context={
+            'departures': departures,
+            'title': title,
+            'subtitle': subtitle,
+            'description': description,
+            'tours': tours,
+            'tours_random_six': tours_random_six
+        })
 
 
 class DepartureView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'departure.html')
+    def get(self, request, departure, *args, **kwargs):
+        dep_amount = 0
+        dep_nights_min = 0
+        dep_nights_max = 0
+        dep_price_min = 0
+        dep_price_max = 0
+        for info in tours.values():
+            if info["departure"] == departure:
+                dep_amount += 1
+                if dep_price_min == 0:
+                    dep_price_min = info["price"]
+                    dep_price_max = info["price"]
+                elif info["price"] < dep_price_min:
+                    dep_price_min = info["price"]
+                elif info["price"] > dep_price_max:
+                    dep_price_max = info["price"]
+                if dep_nights_min == 0:
+                    dep_nights_min = info["nights"]
+                    dep_nights_max = info["nights"]
+                elif info["nights"] < dep_nights_min:
+                    dep_nights_min = info["nights"]
+                elif info["nights"] > dep_nights_max:
+                    dep_nights_max = info["nights"]
+
+        return render(request, 'tours/departure.html', context={
+            "title": title,
+            "departures": departures,
+            "dep_amount": dep_amount,
+            "dep_price_min": dep_price_min,
+            "dep_price_max": dep_price_max,
+            "dep_nights_min": dep_nights_min,
+            "dep_nights_max": dep_nights_max,
+            "tours": tours,
+            "departure": departure,
+            "departure_title": departures[departure][3:]
+        })
 
 
 class TourView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'tour.html')
+    def get(self, request, id, *args, **kwargs):
+        return render(request, 'tours/tour.html', context={
+            "title": title,
+            "departures": departures,
+            "tour_title": tours[id]['title'],
+            "nights": tours[id]['nights'],
+            "price": tours[id]['price'],
+            "description": tours[id]['description'],
+            "stars": tours[id]['stars'],
+            "date": tours[id]['date'],
+            "country": tours[id]['country'],
+            "picture": tours[id]['picture'],
+            "departure_title": departures[tours[id]['departure']][3:]
+        })
